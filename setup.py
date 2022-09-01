9926d14060e8c159d221564b4132c9053f200b69
diiinoya <93127733+diiinoya@users.noreply.github.com>
2022-09-01 14:07:24 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi10.py",
+    version = "0.1",
+    py_modules = ['agi10'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
